f619988edae85ecd9ff3edd46af4d338fe9fe7d1
unknown <akinwumiadekanmi2@gmail.com>
2022-12-28 09:34:35 +0100

    another commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,7 @@
-import { useEffect } from "react";
 import AuthProvider from "../lib/AuthContext";
-import { messaging } from "../lib/firebase-config";
-import { getToken } from "firebase/messaging";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  // async function requestPermission() {
-  //   const permission = Notification.requestPermission();
-  //   if (permission === "granted") {
-  //     const token = await getToken(messaging, {
-  //       vapidKey:
-  //         "BD_1PZpWsGxNRO2YMzK7uYMVboyL0mtvWCfvw3w8r-oZl0nh6_foaeecQPmlpL6LA7_dgunaHQeh62KLaXdMLok",
-  //     });
-  //     console.log("token generated", token);
-  //   } else if (permission === "denied") {
-  //     alert("you denied notification");
-  //   }
-  // }
-  // useEffect(() => {
-  //   requestPermission();
-  // }, []);
   return (
     <AuthProvider>
       <Component {...pageProps} />
